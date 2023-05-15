9dfe74228b43f251a605e2bef374fd1d9fbc035e
fpointzero <fpointzero@foxmail.com>
2023-05-15 22:18:01 +0800

    1

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | Copyright (c) 2023 https://gitee.com/fpointzero/web-course-development All rights reserved.
 // +----------------------------------------------------------------------
-// | Author: 刘洋 | FPoint <fpointzero@foxmail.com>
+// | Author: FPoint <fpointzero@foxmail.com>
 // +----------------------------------------------------------------------
 
 // api 入口
